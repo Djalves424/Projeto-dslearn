f967e4c5d6cb95bf7d485b8062c76b20e86dfd2a
Diego Alves <djalves424@gmail.com>
2024-03-15 16:40:05 -0300

    Created the User classes dslernApplication and dslernAppTests

---
+++ src/main/java/com/djalves/dslearn/DslearnApplication.java
@@ -0,0 +1,12 @@
+package com.djalves.dslearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslearnApplication.class, args);
+	}
+}
