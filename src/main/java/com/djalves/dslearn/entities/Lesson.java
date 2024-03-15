004339448d9587d9b2e0ba18446a89e6b6afc078
Diego Alves <djalves424@gmail.com>
2024-03-15 16:37:52 -0300

    Created the Lesson class

---
+++ src/main/java/com/djalves/dslearn/entities/Lesson.java
@@ -0,0 +1,103 @@
+package com.djalves.dslearn.entities;
+
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.*;
+
+@Entity
+@Table(name = "tb_lesson")
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class Lesson implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private Integer position;
+
+    @ManyToOne
+    @JoinColumn(name = "section_id")
+    private Section section;
+
+    @OneToMany(mappedBy = "lesson")
+    private List<Deliver> deliveries = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "tb_lessons_done",
+            joinColumns = @JoinColumn(name = "lesson_id"),
+            inverseJoinColumns = {
+                    @JoinColumn(name = "user_id"),
+                    @JoinColumn(name = "offer_id")
+            }
+    )
+    private Set<Enrollment> enrollmentDone = new HashSet<>();
+
+    public Lesson() {
+
+    }
+
+    public Lesson(Long id, String title, Integer position, Section section) {
+        this.id = id;
+        this.title = title;
+        this.position = position;
+        this.section = section;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    public Section getSection() {
+        return section;
+    }
+
+    public void setSection(Section section) {
+        this.section = section;
+    }
+
+    public Set<Enrollment> getEnrollmentDone() {
+        return enrollmentDone;
+    }
+
+    public List<Deliver> getDeliveries() {
+        return deliveries;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Lesson lesson = (Lesson) o;
+
+        return Objects.equals(id, lesson.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
