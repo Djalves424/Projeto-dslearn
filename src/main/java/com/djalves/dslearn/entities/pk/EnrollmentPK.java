1b525b3ac2d091a4a1d53967b9cb6a9d11339500
Diego Alves <djalves424@gmail.com>
2024-03-15 16:37:23 -0300

    Created the EnrollmentPK class

---
+++ src/main/java/com/djalves/dslearn/entities/pk/EnrollmentPK.java
@@ -0,0 +1,66 @@
+package com.djalves.dslearn.entities.pk;
+
+import com.djalves.dslearn.entities.Offer;
+import com.djalves.dslearn.entities.User;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class EnrollmentPK implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "offer_id")
+    private Offer offer;
+
+    public EnrollmentPK() {
+
+    }
+
+    public EnrollmentPK(User user, Offer offer) {
+        this.user = user;
+        this.offer = offer;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Offer getOffer() {
+        return offer;
+    }
+
+    public void setOffer(Offer offer) {
+        this.offer = offer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        EnrollmentPK that = (EnrollmentPK) o;
+
+        if (!Objects.equals(user, that.user)) return false;
+        return Objects.equals(offer, that.offer);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = user != null ? user.hashCode() : 0;
+        result = 31 * result + (offer != null ? offer.hashCode() : 0);
+        return result;
+    }
+}
